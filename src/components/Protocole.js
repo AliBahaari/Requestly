2125b30dc0ae0eedc00edb16dd0b8074a6bd76a2
Ali Bahaari <alibahaari1997@gmail.com>
2021-09-25 22:58:22 +0330

    Added Fields Manipulations

--- src/components/Protocole.js
+++ src/components/Protocole.js
@@ -1,26 +1,44 @@
 import Input from "./Input";
 import '../styles/Protocole.css';
+import { useState } from 'react';
 
 function Protocole(props) {
 
+	const [inputElements, setInputElements] = useState([<><Input placeholder="Param Name" /><Input placeholder="Value" /></>]);
+
     const addFields = () => {
+        const component = <><Input placeholder="Param Name" /><Input placeholder="Value" /></>;
+        const addedElements = [...inputElements, component];
+        setInputElements(addedElements);
+    }
 
-        console.log('Add More Fields');
-
-        document.getElementsByClassName(props.className)[0].append(<div>Test</div>);
-
+    const removeFields = () => {
+    	const removedElements = [inputElements.pop()];
+    	setInputElements(removedElements);
     }
 
     return (
         <div className={props.className}>
             <h2>{props.title}</h2>
 
-            <div>
-                <Input placeholder="Param Name" />
-                <Input placeholder="Value" />
-            </div>
+			{
 
-            <span onClick={addFields}>Add More Fields</span>
+				inputElements.map((el, i) => (
+					<div key={i} className="inputElements">
+						{el}
+					</div>
+				))
+
+			}
+
+			{
+				inputElements.length > 1 ?
+					<span className="removeButton" onClick={removeFields}>Remove a Field</span>
+				:
+					''
+			}
+
+            <span className="addButton" onClick={addFields}>Add a Field</span>
         </div>
     );
 }
